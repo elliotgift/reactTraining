3faa4e3a88f583f98270105fd958de26ae7afe80
unknown <giftyelliot2@gmail.com>
2020-08-11 15:45:00 +0000

    Updates

---
+++ precache-manifest.e9f95326eaf98df7ce1341a5bfced93e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ac5f8668adc458c8de1a25a2c92eb75",
+    "url": "/elliotgift/reactTraining.git/index.html"
+  },
+  {
+    "revision": "bf4e3a5c501d76965373",
+    "url": "/elliotgift/reactTraining.git/static/css/main.35d64166.chunk.css"
+  },
+  {
+    "revision": "aa6984cd07520a83fd66",
+    "url": "/elliotgift/reactTraining.git/static/js/2.0a57a6d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/elliotgift/reactTraining.git/static/js/2.0a57a6d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf4e3a5c501d76965373",
+    "url": "/elliotgift/reactTraining.git/static/js/main.bcfb9275.chunk.js"
+  },
+  {
+    "revision": "53d62b12771aa2c886b4",
+    "url": "/elliotgift/reactTraining.git/static/js/runtime-main.7ce2a647.js"
+  },
+  {
+    "revision": "7c72ad7b7ddb0986fa135a3cab834b42",
+    "url": "/elliotgift/reactTraining.git/static/media/img1.7c72ad7b.jpg"
+  },
+  {
+    "revision": "deacf3238a02a9771a87d8ff5e973392",
+    "url": "/elliotgift/reactTraining.git/static/media/img2.deacf323.jpg"
+  },
+  {
+    "revision": "2f8afe5cbaf6e7d608b94584cd6ff9fc",
+    "url": "/elliotgift/reactTraining.git/static/media/img3.2f8afe5c.jpg"
+  }
+]);
